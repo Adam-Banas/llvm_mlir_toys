81a64a10714e9201259f11ef2c0f18832aa996e2
Adam Banas <adam.banas.1989@gmail.com>
2024-02-07 10:42:01 +0100

    Fix clangd compile_commands.json setup

--- toy/build.sh
+++ toy/build.sh
@@ -6,5 +6,9 @@ LLVM_INSTALL_DIR=/usr/local
 
 # Commands
 mkdir build && cd build
-cmake -G Ninja .. -DMLIR_DIR=$LLVM_INSTALL_DIR/lib/cmake/mlir -DLLVM_EXTERNAL_LIT=$LLVM_BUILD_DIR/bin/llvm-lit -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_CXX_FLAGS="-g"
+cmake -G Ninja .. \
+-D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_CXX_STANDARD=17 \
+-DMLIR_DIR=$LLVM_INSTALL_DIR/lib/cmake/mlir -DLLVM_EXTERNAL_LIT=$LLVM_BUILD_DIR/bin/llvm-lit \
+-DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+-DCMAKE_CXX_FLAGS="-g"
 cmake --build .
